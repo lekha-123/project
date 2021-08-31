64c71ffb892b0a35367505d3089d7fa310cb5af4
lekha-123 <71594197+lekha-123@users.noreply.github.com>
2021-08-31 18:25:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+
+
+var students={
+  name:'sahil',
+  rollno:12,
+  grade:7,
+  address:'singapore',
+  school:'js international school'
+} 
+function setup() {
+  createCanvas(400, 400);
+  console.log(students.rollno);
+  console.log(students.school);
+  console.log(students.address);
+}
+
+function draw() {
+  background(220);
+}
